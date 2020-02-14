2e3aa2fe76c6991bf860d9a1547400c3e56a6a23
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-02-14 16:27:52 -0500

    add BookmakerFeed

---
+++ BasketballData.Processors/ApiBasketball/Feeds/BookmakerFeed.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasketballData.Processors.ApiBasketball.Feeds
+{
+	public class BookmakerFeed
+	{
+		public static string GetFeedUrl()
+		{
+			return "https://api-basketball.p.rapidapi.com/bookmakers";
+		}
+
+		public static BookmakerFeed FromJson(string json) => JsonConvert.DeserializeObject<BookmakerFeed>(json, Converter.Settings);
+
+		[JsonProperty("get")]
+		public string Get { get; set; }
+
+		[JsonProperty("parameters")]
+		public List<object> Parameters { get; set; }
+
+		[JsonProperty("errors")]
+		public List<object> Errors { get; set; }
+
+		[JsonProperty("results")]
+		public int Count { get; set; }
+
+		[JsonProperty("response")]
+		public List<Bookmaker> Bookmakers { get; set; }
+
+		public class Bookmaker
+		{
+			[JsonProperty("id")]
+			public int Id { get; set; }
+
+			[JsonProperty("name")]
+			public string Name { get; set; }
+		}
+	}
+
+	public static partial class Serialize
+	{
+		public static string ToJson(this BookmakerFeed self) => JsonConvert.SerializeObject(self, BasketballData.Processors.ApiBasketball.Feeds.Converter.Settings);
+	}
+}
